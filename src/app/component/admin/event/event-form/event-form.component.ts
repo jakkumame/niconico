4da391908f9fb6e2a2bcde3ef6c709d8246bd81a
jakkumame <nkmrysyk0914@gmail.com>
2023-08-28 19:58:17 +0900

    event-form service

--- src/app/component/admin/event/event-form/event-form.component.ts
+++ src/app/component/admin/event/event-form/event-form.component.ts
@@ -1,9 +1,7 @@
-// event-form.component.ts
-
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { EventFormService } from 'src/app/service/event/event-form.service';
-import { AlertController } from '@ionic/angular';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-event-form',
@@ -11,16 +9,20 @@ import { AlertController } from '@ionic/angular';
   styleUrls: ['./event-form.component.scss']
 })
 export class EventFormComponent implements OnInit {
-
   eventForm!: FormGroup;
 
   constructor(
     private fb: FormBuilder,
     private eventFormService: EventFormService,
-    private alertController: AlertController
-  ) { }
+    private alertCtrl: AlertController,
+    private navCtrl: NavController,
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  private initializeForm(): void {
     this.eventForm = this.fb.group({
       date: ['', Validators.required],
       startTime: ['', Validators.required],
@@ -29,35 +31,29 @@ export class EventFormComponent implements OnInit {
     });
   }
 
-  async onSubmit() {
+  async onSubmit(): Promise<void> {
     if (this.eventForm.valid) {
-        try {
-            await this.eventFormService.addEvent(this.eventForm.value);
-            this.eventForm.reset();
-            const alert = await this.alertController.create({
-                header: '成功',
-                message: 'こども食堂の新しい開催詳細が作成されました',
-                buttons: ['OK']
-            });
-            await alert.present();
-        } catch (error: unknown) {
-            let errorMessage: string;
-
-            if (error instanceof Error) {
-                errorMessage = error.message;
-            } else {
-                errorMessage = '予期せぬエラーが発生しました。';
-            }
-
-            const alert = await this.alertController.create({
-                header: 'エラー',
-                message: errorMessage,
-                buttons: ['OK']
-            });
-            await alert.present();
-        }
+      try {
+        await this.eventFormService.addEvent(this.eventForm.value);
+        this.showAlert('成功', 'こども食堂の新しい開催詳細が作成されました');
+        this.eventForm.reset();
+      } catch (error: unknown) {
+        const errorMessage = error instanceof Error ? error.message : '予期せぬエラーが発生しました。';
+        this.showAlert('エラー', errorMessage);
+      }
     }
-}
+  }
 
+  private async showAlert(header: string, message: string): Promise<void> {
+    const alert = await this.alertCtrl.create({
+      header,
+      message,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
 
+  goBack(): void {
+    this.navCtrl.back();
+  }
 }
